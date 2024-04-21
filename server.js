266780377f9bd348ebb5d49ad4e09e5948090e9d
Mikey-Campion <152976804+Mikey-Campion@users.noreply.github.com>
2024-04-21 18:01:11 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,13 +3,16 @@ const protoLoader = require('@grpc/proto-loader');
 const { randomInt } = require('crypto');
 const packageDefinition = protoLoader.loadSync('city.proto',{});
 const cityProto = grpc.loadPackageDefinition(packageDefinition).city; 
+const heavy_traffic = ["M50 Southbound","M50 Northbound","N11","South Quays"];
 
 
-function count(call,callback){
-    let roadList = call.request.road;
+function count(call){
+    let roadList =  ["South Circular", "O'Connell St","Westmoreland St", "Upper Rathmines Rd", "M50 Southbound"];
     let message = "";
     let warning = "Normal traffic conditions";
     let total_cars = 0;
+
+    call.write({count:message});
 
     for (let i = 0; i < roadList.length; i++){
         message = randomInt(13,500);
@@ -18,41 +21,50 @@ function count(call,callback){
         call.write({count:message});
     }
     
-  /*  if ((total_cars/roadList.length)>100){
-        warning = "Warning: Heavy traffic";
-    } */
-
-  //  call.write({traffic_warning:warning});
-
     call.end();
 } 
 
 function lights(call,callback){
-    let status = call.request.status;
-    let energy = 0;
+    let status ="";
+    let energy = randomInt(50000,500000);
     console.log("lights function is running");
 
-    if (call.request.toggle){
-        if (status.match("Off")){
-            status = "On";
-        }
-        else{
-            status = "Off";
-        }
+    if (call.request.toggle == "On"){
+        status = "On";
     }
-
+    else{
+        status = "Off";
+    }
    
     callback(null,{status:status,energy:energy});
     
+}
+
+function warning(call,callback){
+    let message = "\nWarning Message\n---------------------------------------------------------------------\n";
+
+    call.on("data",function(request){
+        for (let i = 0; i<heavy_traffic.length; i++){
+            if (request.location == heavy_traffic[i]){
+                message = message + "Heavy Traffic warning: " + request.location + "\n"
+            }
+        }
+    });
+
+    call.on("end", function () {
+        callback(null, {warning:message});
+    });
 }
 
 function main(){
     const server = new grpc.Server();
     server.addService(cityProto.Lights.service,{switch:lights});
     server.addService(cityProto.Counter.service,{counter:count});
+    server.addService(cityProto.TrafficWarning.service,{warning:warning});
     server.bindAsync('0.0.0.0:4500', grpc.ServerCredentials.createInsecure(), () => {
    // server.start();
     console.log('Server running at http://0.0.0.0:4500');
     });
+
 }
 main();
