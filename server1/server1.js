c57fbefdcf932fd195185dfc5262bfd46239b3a3
Mikey-Campion <152976804+Mikey-Campion@users.noreply.github.com>
2024-04-23 11:29:42 +0100

    Comments added to server1.js

--- server1/server1.js
+++ server1/server1.js
@@ -1,34 +1,44 @@
+/*
+ * Server program for smart city project
+ * This server implements the Counter grpc service
+ * Server side streaming is used here
+ * Author: Michael Campion
+ */
+
+// This section loads the required proto file
 const grpc = require('@grpc/grpc-js');
 const protoLoader = require('@grpc/proto-loader'); 
-const { randomInt } = require('crypto');
+const { randomInt } = require('crypto'); // Used to create the data that is sent to the client
 const packageDefinition = protoLoader.loadSync('count.proto',{});
 const countProto = grpc.loadPackageDefinition(packageDefinition).count; 
 
 function main(){
+    //This section creates the server and the connection with the client
     const server = new grpc.Server();
     server.addService(countProto.Counter.service,{counter:count});
     server.bindAsync('0.0.0.0:4500', grpc.ServerCredentials.createInsecure(), () => {
-   // server.start();
     console.log('Server running at http://0.0.0.0:4500');
     });
 
 }
 main();
 
+//This function is the implementation of the Counter service
 function count(call){
+    // List of roads that have "sensors" to measure traffic numbers
     let roadList =  ["South Circular", "O'Connell St","Westmoreland St", "Upper Rathmines Rd", "M50 Southbound"];
-    let message = "";
-    let warning = "Normal traffic conditions";
+    let message = ""; // Message that will be sent to the client
     let total_cars = 0;
 
     call.write({count:message});
 
+    // This loop sends a message with traffic statistics for each road in the list
     for (let i = 0; i < roadList.length; i++){
-        message = randomInt(13,500);
+        message = randomInt(13,500); // Create a random number of cars that have passed to simulate a sensor
         total_cars = total_cars + message;
-        message = message +" cars passed " + roadList[i] + " sensor in the past 30 mins";
-        call.write({count:message});
+        message = message +" cars passed " + roadList[i] + " sensor in the past 30 mins"; // Create message to be sent to client
+        call.write({count:message}); // Send message to the client
     }
     
-    call.end();
+    call.end(); // End call once streaming is finished
 }
